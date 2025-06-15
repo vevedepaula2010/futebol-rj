8baa9cf9b6427960ade36e23fd106d12e1eca844
vevedepaula2010 <vevedepaula2010@gmail.com>
2025-06-15 17:26:00 -0300

    fix: atualiza links de navegação e substitui logo

--- includes/nav.php
+++ includes/nav.php
@@ -1,16 +1,19 @@
 <link rel="stylesheet" href="/style/nav.css">
 <nav class="navbar">
     <div class="container navbar-container">
-        <a href="home.php" class="logo navbar-logo">
+        <a href="/page/home.php" class="logo navbar-logo">
             <img src="/img/logo.png">
         </a>
         <ul class="nav-links navbar-menu">
-            <li class="navbar-item"><a href="jogadores/listar.php" class="navbar-link">Jogadores</a></li>
-            <li class="navbar-item"><a href="times/listar.php" class="navbar-link">Times</a></li>
-            <li class="navbar-item"><a href="jogos/listar.php" class="navbar-link">Jogos</a></li>
-            <li class="navbar-item"><a href="lesoes/listar.php" class="navbar-link">Lesões</a></li>
-            <li class="navbar-item"><a href="performance/listar.php" class="navbar-link">Performance</a></li>
-            <li class="navbar-item"><a href="jogadores/cadastrar.php" class="navbar-link">Cadastrar Jogador</a></li>
+            <li class="navbar-item"><a href="/page/jogadores/listar.php" class="navbar-link">Jogadores</a></li>
+            <li class="navbar-item"><a href="/page/times/listar.php" class="navbar-link">Times</a></li>
+            <li class="navbar-item"><a href="/page/jogos/listar.php" class="navbar-link">Jogos</a></li>
+            <li class="navbar-item"><a href="/page/jogos/listar.php" class="navbar-link">Jogos</a></li>
+            <li class="navbar-item"><a href="/page/lesoes/listar.php" class="navbar-link">Lesões</a></li>
+            <li class="navbar-item"><a href="/page/performance/listar.php" class="navbar-link">Performance</a></li>
+            <li class="navbar-item"><a href="/page/jogadores/cadastrar.php" class="navbar-link">Cadastrar Jogador</a></li>
         </ul>
     </div>
-</nav>  
+</nav>
+
+
